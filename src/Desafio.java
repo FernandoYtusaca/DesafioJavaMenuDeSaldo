153d697be937ad332722202ba70756037ad8c7ee
Fernando Ytusaca Quispe <fernando.ytusaca@gmail.com>
2026-04-08 11:14:07 -0500

    Add files via upload

---
+++ src/Desafio.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre = "Tony Stark";
+        String tipoDeCuenta = "Corriente";
+        double saldo = 1599.99;
+        int opcion = 0;
+
+        System.out.println("*****************************************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("Tipo de cuenta: " + tipoDeCuenta);
+        System.out.println("Saldo disponible: " + saldo);
+        System.out.println("\n*****************************************");
+
+        String menu = """
+                *** Escriba el número de la opción deseada ***
+                1 - Consultar Saldo
+                2 - Retirar
+                3 - Depositar
+                9 - Salir
+                """;
+        Scanner teclado = new Scanner(System.in);
+        while (opcion != 9){
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Su saldo actual es: " + saldo + "$");
+                    break;
+                case 2:
+                    System.out.println("¿Cuál es el valor que desea retirar?");
+                    double valorARetirar = teclado.nextDouble();
+                    if (saldo < valorARetirar) {
+                        System.out.println("Saldo insuficiente");
+                    }else {
+                        saldo =saldo - valorARetirar;
+                        System.out.println("El saldo actualizado es: " + saldo);
+                    }
+
+                    break;
+                case 3:
+                    System.out.println("¿Cuál es el valor que desea depositar?");
+                    double valorADepositar = teclado.nextDouble();
+                    saldo += valorADepositar;
+                    System.out.println("El saldo actualizado es: " + saldo);
+                    break;
+                case 9:
+                    System.out.println("Gracias por utilizar nuestro servicio");
+                    break;
+                default:
+                    System.out.println("Opción no valida");
+            }
+        }
+    }
+}
